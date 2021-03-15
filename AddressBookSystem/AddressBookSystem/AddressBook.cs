5d7a4632540e815a0a5a562dec4b837e8dd8a4bb
unknown <nandiniswaraj13@gmail.com>
2021-03-15 23:21:58 +0530

    [Nandiniswaraj]Added Edit Record

--- AddressBookSystem/AddressBookSystem/AddressBook.cs
+++ AddressBookSystem/AddressBookSystem/AddressBook.cs
@@ -68,5 +68,95 @@ namespace AddressBookSystem
             contactDetails(bookName);
         }
 
+        public static void editRecord(string recordNameToEdit, string bookToEdit)
+        {
+
+            if (Program.addressBookStore.ContainsKey(bookToEdit))
+            {
+                List<Contact> book = Program.addressBookStore[bookToEdit];
+                foreach (Contact record in book)
+                {
+                    if (book.Contains(record))
+                    {
+                        Console.WriteLine("Select Which Data You Want To Update \n1.First_Name \n2.Last_Name \n3.Address" +
+                                               "\n4.City \n5.State \n6.Zip \n7.PhoneNumber \n8.Email");
+                        string selection = Console.ReadLine();
+                        string newData;
+                        switch (selection)
+                        {
+                            case "1":
+                                Console.WriteLine("Enter First_Name");
+                                newData = Console.ReadLine();
+                                record.first_name = newData;
+                                Console.WriteLine("Your Updated  Records :\n" + record.toString());
+
+                                break;
+                            case "2":
+                                Console.WriteLine("Enter Last_Name");
+                                newData = Console.ReadLine();
+                                record.last_name = newData;
+                                Console.WriteLine("Your Updated  Records :\n" + record.toString());
+
+                                break;
+                            case "3":
+                                Console.WriteLine("Enter Address");
+                                newData = Console.ReadLine();
+                                record.address = newData;
+                                Console.WriteLine("Your Updated  Records :\n" + record.toString());
+
+                                break;
+                            case "4":
+                                Console.WriteLine("Enter City");
+                                newData = Console.ReadLine();
+                                record.city = newData;
+                                Console.WriteLine("Your Updated  Records :\n" + record.toString());
+
+                                break;
+                            case "5":
+                                Console.WriteLine("Enter State");
+                                newData = Console.ReadLine();
+                                record.state = newData;
+                                Console.WriteLine("Your Updated  Records :\n" + record.toString());
+
+                                break;
+                            case "6":
+                                Console.WriteLine("Enter Zip");
+                                newData = Console.ReadLine();
+                                record.zip = newData;
+                                Console.WriteLine("Your Updated  Records :\n" + record.toString());
+
+                                break;
+                            case "7":
+                                Console.WriteLine("Enter PhoneNumber");
+                                newData = Console.ReadLine();
+                                record.phoneNumber = newData;
+                                Console.WriteLine("Your Updated  Records :\n" + record.toString());
+
+                                break;
+                            case "8":
+                                Console.WriteLine("Enter Email");
+                                newData = Console.ReadLine();
+                                record.email = newData;
+                                Console.WriteLine("Your Updated  Records :\n" + record.toString());
+
+                                break;
+                            default:
+                                Console.WriteLine("Invalid Selection Input");
+                                break;
+                        }
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("No Such BookAddress Found");
+            }
+
+
+
+        }
+
+
     }
 }
--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -14,6 +14,13 @@ namespace AddressBookSystem
             Console.WriteLine("Enter name of the address book in which you want to add record.");
             String bookName = Console.ReadLine();
             AddressBook.addRecord(bookName);
+            Console.WriteLine("Enter the name fo the address book you want to edit.");
+            String bookToEdit = Console.ReadLine();
+            Console.WriteLine("Enter the first name of the person whose record you want to edit.");
+            String recordNameToEdit = Console.ReadLine();
+            AddressBook.editRecord(recordNameToEdit, bookToEdit);
+            
+
 
 
         }
